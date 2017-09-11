0db6299952f25378cde424d0626426844d24a373
Rafael Magossi <rafael_mq2@hotmail.com>
2017-09-11 00:23:10 -0400

    Configurando estrutura do Projeto

---
+++ js/config.js
@@ -0,0 +1,9 @@
+function setConfig(){
+    var textos = {
+        "titulo": "Controle de Compra"
+    };
+    document.title = textos.titulo;
+    document.getElementById("navTitle").innerHTML = textos.titulo;
+}
+
+setConfig();
